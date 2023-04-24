7e0553a4bc4d477ca8af607e7970036b10d01040
alexandru.badea <Alexandru.Badea@tss-yonder.com>
2023-04-24 14:49:01 +0300

    ok

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { WriteCodeComponent } from './pages/write-code/write-code.component';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { AuxiliaryComponent } from './pages/auxiliary/auxiliary.component';
 
 @NgModule({
-  declarations: [HomeComponent, WriteCodeComponent],
+  declarations: [HomeComponent, WriteCodeComponent, AuxiliaryComponent],
   imports: [CommonModule, HomeRoutingModule, ReactiveFormsModule],
 })
 export class HomeModule {}
---
+++ src/app/home/pages/auxiliary/auxiliary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auxiliary',
+  templateUrl: './auxiliary.component.html',
+  styleUrls: ['./auxiliary.component.scss']
+})
+export class AuxiliaryComponent {
+
+}
